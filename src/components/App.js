4ce4fb54e3e661ee09edd582ca831747bd5bd709
Alex Reany <reanyalex@gmail.com>
2020-08-13 21:08:07 -0400

    Added Navbar and basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Landing from "./Landing";
+import TopNav from './navigation/TopNav'
+import Recipes from './recipes/Recipes'
+import { Container } from 'semantic-ui-react'
 
 const App = () => {
-    return <div>App</div>
+    return (<div>
+        <Router>
+            <Container >
+                <TopNav />
+                <Switch>
+                    <Route path="/recipes">
+                        <Recipes />
+                    </Route>
+                    <Route path="/">
+                        <Landing />
+                    </Route>
+                </Switch>
+            </Container>
+        </Router>
+    </div>
+    )
 };
 
 export default App;
